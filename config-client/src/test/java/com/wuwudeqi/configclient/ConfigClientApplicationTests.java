a7cd16bf6d50ce2627a4d235804ba5adb2cadaf2
富佳森 <920776574@qq.com>
2020-03-13 10:58:41 +0800

    conf-client添加

---
+++ config-client/src/test/java/com/wuwudeqi/configclient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuwudeqi.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
